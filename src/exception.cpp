b0f557e6df4dd7078067a3c31588c8a10bd8a0b6
黄融亮 <21373488@buaa.edu.cn>
2023-10-25 16:02:49 +0800

    v3.4

--- src/exception.cpp
+++ src/exception.cpp
@@ -2,19 +2,38 @@
 
 #define exception_debug_output
 
+std::string ErrorCodeString[] = {
+    "IllegalCharInFormatString",
+    "Redefine",
+    "Nodefine",
+    "WrongParameterNumber",
+    "WrongParameterType",
+    "SuperfluousReturnValue",
+    "MissingReturnStatement",
+    "ModifyConst",
+    "MissingSemicolon",
+    "MissingCloseParentheses",
+    "MissingCloseSquareBracket",
+    "UnmatchedPrintArgs",
+    "BreakOrContinueOutOfLoop",
+    "UnknownToken",
+    "OtherError",
+    "MissingReturnValue",
+    "CalculateTypeNotMatch",
+};
 
 std::ostream& operator<<(std::ostream& fout, const CompilerException& compilerException)
 {
-    // if (&fout == &std::cout)
-    //     fout << "\033[1;31m"
-    //          << "in line\t" << compilerException.line << "\tcolumn\t" << compilerException.column << '\t' << 'a' + static_cast<int>(compilerException.type) << "\033[0m";
-    // else
-    //     fout << "in line\t" << compilerException.line << "\tcolumn\t" << compilerException.column << '\t' << 'a' + static_cast<int>(compilerException.type);
     if (&fout == &std::cout)
         fout << "\033[1;31m"
-             << compilerException.line << ' ' << (char)('a' + static_cast<int>(compilerException.type)) << "\033[0m";
+             << "in line\t" << compilerException.line << "\tcolumn\t" << compilerException.column << '\t' << ErrorCodeString[static_cast<int>(compilerException.type)] << "\033[0m";
     else
-        fout << compilerException.line << ' ' << (char)('a' + static_cast<int>(compilerException.type));
+        fout << "in line\t" << compilerException.line << "\tcolumn\t" << compilerException.column << '\t' << ErrorCodeString[static_cast<int>(compilerException.type)];
+    // if (&fout == &std::cout)
+    //     fout << "\033[1;31m"
+    //          << compilerException.line << ' ' << (char)('a' + static_cast<int>(compilerException.type)) << "\033[0m";
+    // else
+    //     fout << compilerException.line << ' ' << (char)('a' + static_cast<int>(compilerException.type));
     return fout;
 }
 std::string CompilerException::toString() const
